c766aee9a6607b84cd6ac76578a066822ba51077
AnilBASARAN <nlbsrn@gmail.com>
2024-10-08 20:16:22 +0300

    first commit

---
+++ day7.js
@@ -0,0 +1,30 @@
+/* CHALLANGE day 7:
+
+pair product
+
+Write a function, pairProduct, that takes in an array and a target product as arguments.
+The function should return an array containing a pair of indices whose elements multiply to the given target. 
+The indices returned must be unique.
+
+Be sure to return the indices, not the elements themselves.
+There is guaranteed to be one such pair whose product is the target. */
+
+const pairProduct = (numbers, targetProduct) => {
+
+    let previousNumbersObj = {};
+    
+    for(let i = 0; i < numbers.length ; i++){
+      const number = numbers[i];
+      const complementNumber = targetProduct / number;
+      if(complementNumber in previousNumbersObj){
+        return [previousNumbersObj[complementNumber],i];
+      }else{
+        previousNumbersObj[number] = i;
+      }
+    }
+    // todo
+  };
+  
+  module.exports = {
+    pairProduct,
+  };
